e7004988d5ba6505d3f5466cdf78b7bedc49807f
takeurpills <toth.martin@hotmail.sk>
2021-08-14 20:38:30 +0200

    Raw full scrape implemented

--- selenium_tutorial.py
+++ selenium_tutorial.py
@@ -19,7 +19,6 @@ time.sleep(LOAD_PAUSE_TIME)
 # initialize the storage variables
 teams = []
 x12 = [] #3-way score X 1 2
-odds_events = []
 
 # turn off live odds
 filter_button = driver.find_element_by_xpath('//*[@id="left-column"]/div/div/div/div/ul/li[3]')
@@ -41,3 +40,31 @@ while (len(end_of_page) < 1):
     scroll_context_selector.send_keys(Keys.END)
     end_of_page = driver.find_elements_by_xpath("//*[contains(text(), 'Prejsť na mobilnú verziu')]")
     time.sleep(SCROLL_PAUSE_TIME)
+
+#select all standard matches, excluding superchance and other special promotions
+leagues = driver.find_elements_by_xpath('//*[contains(@data-atid, "n1-league-box")]')
+
+#loop through all leagues, filter for only classic 1X2 matches and extract data match by match
+for league in leagues:
+    if league.find_element_by_xpath('.//div[@class="content-box-bets-subtitle no-wrap bet"]').text == "ZÁPAS":
+        matches_parent = league.find_element_by_xpath('../../..')
+        matches = league.find_elements_by_xpath('.//div[@class="flex bet-view-prematch-row"]')
+
+        for match in matches:
+            match_info_raw = match.get_attribute("title")
+            match_info_list = match_info_raw.replace("\n"," | ").split(" | ")
+            teams.append(match_info_list[3])
+
+            match_odds_parents = match.find_elements_by_xpath('.//a[@data-atid="n1-bet-box"]')
+            match_odds_list = []
+            odds = ""
+            for match_odds in match_odds_parents:                
+                odds = match_odds.find_element_by_xpath('.//span[@class="text-extra-bold"]').text
+                if len(odds) > 1:
+                    match_odds_list.append(odds)
+                else:
+                    match_odds_list.append("1.00")
+            
+            x12.append(match_odds_list)
+
+print(x12)
